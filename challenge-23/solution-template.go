b24474da22bb8dc0321ef07d5ad2de5eecb3eb56
mick4711 <mick4711@gmail.com>
2025-07-10 00:11:20 +0100

    naive

--- challenge-23/solution-template.go
+++ challenge-23/solution-template.go
@@ -41,8 +41,38 @@ func main() {
 // NaivePatternMatch performs a brute force search for pattern in text.
 // Returns a slice of all starting indices where the pattern is found.
 func NaivePatternMatch(text, pattern string) []int {
-	// TODO: Implement this function
-	return nil
+	// check empty strings
+	if text == "" || pattern == "" {
+		return []int{}
+	}
+
+	// get lens and make result slice
+	lt := len(text)
+	lp := len(pattern)
+	res := make([]int, 0, lt/lp)
+
+	// loop thru text
+	for i := 0; i <= lt-lp; i++ {
+		// check each char in pattern
+		k := i
+		for j := 0; j < lp; {
+			if text[k:k+1] == pattern[j:j+1] {
+				// chars match move along
+				k++
+				j++
+			} else {
+				// chars don't match stop this pattern match
+				break
+			}
+		}
+
+		// check if pattern matched by count of loop
+		if k-i == lp {
+			res = append(res, i)
+		}
+	}
+
+	return res
 }
 
 // KMPSearch implements the Knuth-Morris-Pratt algorithm to find pattern in text.
