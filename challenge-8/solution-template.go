3f8931d25418f742e72241946f1dbb4bb7072ad6
mick4711 <mick4711@gmail.com>
2025-07-25 22:58:43 +0100

    working it out

--- challenge-8/solution-template.go
+++ challenge-8/solution-template.go
@@ -3,6 +3,7 @@ package challenge8
 
 import (
 	"errors"
+	"fmt"
 	"sync"
 )
 
@@ -11,23 +12,38 @@ type Client struct {
 	// username, message channel, mutex, disconnected flag
 	username     string
 	messages     chan string
-	server       *ChatServer
 	mu           sync.Mutex
 	disconnected bool
 }
 
 // Send sends a message to the client
 func (c *Client) Send(message string) {
-	// TODO: Implement this method
-	// Hint: thread-safe, non-blocking send
-	c.messages <- message
+	if c.disconnected {
+		return
+	}
+
+	// thread-safe
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	// non-blocking
+	select {
+	case c.messages <- message:
+		fmt.Printf("Client [%s] has been SENT message:[%s]\n", c.username, message)
+	default:
+	}
 }
 
 // Receive returns the next message for the client (blocking)
 func (c *Client) Receive() string {
-	// TODO: Implement this method
-	// Hint: read from channel, handle closed channel
-	return <-c.messages
+	// read from channel, handle closed channel
+	if message, ok := <-c.messages; ok {
+		fmt.Printf("Client [%s] RECEIVED message:[%s]\n", c.username, message)
+		return message
+	}
+
+	fmt.Printf("Client [%s] no more messages\n", c.username)
+	return "no more messages"
 }
 
 type BroadcastMessage struct {
@@ -55,40 +71,50 @@ func NewChatServer() *ChatServer {
 // Connect adds a new client to the chat server
 func (s *ChatServer) Connect(username string) (*Client, error) {
 	// check username, create client, add to map
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	if _, ok := s.clients[username]; ok {
 		return nil, ErrUsernameAlreadyTaken
 	}
 
 	client := &Client{
 		username:     username,
-		messages:     make(chan string),
-		server:       s,
-		mu:           sync.Mutex{},
+		messages:     make(chan string,1), //TODO see if working with no buffer
 		disconnected: false,
 	}
 	s.clients[username] = client
-	// TODO  s.connect <- client
 
 	return client, nil
 }
 
 // Disconnect removes a client from the chat server
 func (s *ChatServer) Disconnect(client *Client) {
-	// TODO: Implement this method
-	// Hint: remove from map, close channels
+	// remove from map, close channels
 	delete(s.clients, client.username)
+	close(client.messages)
 }
 
 // Broadcast sends a message to all connected clients
 func (s *ChatServer) Broadcast(sender *Client, message string) {
-	// TODO: Implement this method
-	// Hint: format message, send to all clients
+	// TODO: mutex?
+	// format message, send to all clients
+	for _, client := range s.clients {
+		client.Send(fmt.Sprintf("From:%s\nMessage:%s\n", sender.username, message))
+	}
 }
 
 // PrivateMessage sends a message to a specific client
 func (s *ChatServer) PrivateMessage(sender *Client, recipient string, message string) error {
-	// TODO: Implement this method
-	// Hint: find recipient, check errors, send message
+	// find recipient, check errors, send message
+	receiver, ok := s.clients[recipient]
+	if !ok {
+		return ErrRecipientNotFound
+	}
+
+	fmt.Printf("Sender [%s] sending to:[%s] private message: [%s]\n", sender.username, recipient, message)
+	receiver.Send(fmt.Sprintf("PRIVATE message from [%s] to:[%s] content: %s", sender.username, recipient, message))
+
 	return nil
 }
 
