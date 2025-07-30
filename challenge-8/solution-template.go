6e47b1e72ae4e5dc7b83204f7126d2b6aa1a42ac
Michael O'Hegarty <mick4711@gmail.com>
2025-07-30 18:02:25 +0100

    Done

--- challenge-8/solution-template.go
+++ challenge-8/solution-template.go
@@ -3,130 +3,63 @@ package challenge8
 
 import (
 	"errors"
-	"fmt"
-	"sync"
+	// Add any other necessary imports
 )
 
 // Client represents a connected chat client
 type Client struct {
-	// username, message channel, mutex, disconnected flag
-	username     string
-	messages     chan string
-	mu           sync.Mutex
-	disconnected bool
+	// TODO: Implement this struct
+	// Hint: username, message channel, mutex, disconnected flag
 }
 
 // Send sends a message to the client
 func (c *Client) Send(message string) {
-	if c.disconnected {
-		return
-	}
-
-	// thread-safe
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	c.messages <- message
-	fmt.Printf("Client [%s] has been SENT message:[%s]\n", c.username, message)
-	// non-blocking
-	// select {
-	// case c.messages <- message:
-	// 	fmt.Printf("Client [%s] has been SENT message:[%s]\n", c.username, message)
-	// default:
-	// 	fmt.Printf("default NOT sent\n")
-	// }
+	// TODO: Implement this method
+	// Hint: thread-safe, non-blocking send
 }
 
 // Receive returns the next message for the client (blocking)
 func (c *Client) Receive() string {
-	// read from channel, handle closed channel
-	if message, ok := <-c.messages; ok {
-		fmt.Printf("Client [%s] RECEIVED message:[%s]\n", c.username, message)
-		return message
-	}
-
-	fmt.Printf("Client [%s] no more messages\n", c.username)
-	return "no more messages"
-
-	// select {
-	// case message := <-c.messages:
-	// 	fmt.Printf("Client [%s] RECEIVED message:[%s]\n", c.username, message)
-	// 	return message
-	// 	// fmt.Printf("Client [%s] has been SENT message:[%s]\n", c.username, message)
-	// default:
-	// 	return "no more messages"
-	// }
-}
-
-type BroadcastMessage struct {
-	sender *Client
-	msg    string
+	// TODO: Implement this method
+	// Hint: read from channel, handle closed channel
+	return ""
 }
 
 // ChatServer manages client connections and message routing
 type ChatServer struct {
-	// clients map, mutex
-	clients    map[string]*Client
-	broadcast  chan BroadcastMessage
-	connect    chan *Client
-	disconnect chan *Client
-	mu         sync.Mutex
+	// TODO: Implement this struct
+	// Hint: clients map, mutex
 }
 
 // NewChatServer creates a new chat server instance
 func NewChatServer() *ChatServer {
-	return &ChatServer{
-		clients: make(map[string]*Client),
-	}
+	// TODO: Implement this function
+	return nil
 }
 
 // Connect adds a new client to the chat server
 func (s *ChatServer) Connect(username string) (*Client, error) {
-	// check username, create client, add to map
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	if _, ok := s.clients[username]; ok {
-		return nil, ErrUsernameAlreadyTaken
-	}
-
-	client := &Client{
-		username:     username,
-		messages:     make(chan string),
-		disconnected: false,
-	}
-	s.clients[username] = client
-
-	return client, nil
+	// TODO: Implement this method
+	// Hint: check username, create client, add to map
+	return nil, nil
 }
 
 // Disconnect removes a client from the chat server
 func (s *ChatServer) Disconnect(client *Client) {
-	// remove from map, close channels
-	delete(s.clients, client.username)
-	close(client.messages)
+	// TODO: Implement this method
+	// Hint: remove from map, close channels
 }
 
 // Broadcast sends a message to all connected clients
 func (s *ChatServer) Broadcast(sender *Client, message string) {
-	// TODO: mutex?
-	// format message, send to all clients
-	for _, client := range s.clients {
-		client.Send(fmt.Sprintf("From:%s\nMessage:%s\n", sender.username, message))
-	}
+	// TODO: Implement this method
+	// Hint: format message, send to all clients
 }
 
 // PrivateMessage sends a message to a specific client
 func (s *ChatServer) PrivateMessage(sender *Client, recipient string, message string) error {
-	// find recipient, check errors, send message
-	receiver, ok := s.clients[recipient]
-	if !ok {
-		return ErrRecipientNotFound
-	}
-
-	fmt.Printf("Sender [%s] sending to:[%s] private message: [%s]\n", sender.username, recipient, message)
-	receiver.Send(fmt.Sprintf("PRIVATE message from [%s] to:[%s] content: %s", sender.username, recipient, message))
-
+	// TODO: Implement this method
+	// Hint: find recipient, check errors, send message
 	return nil
 }
 
